f5a24b39058eed8990fc98e25dda9d691288b5f7
hamed <shiani@outlook.com>
2022-09-09 21:33:11 +0430

    fix a bug in AddProjectPermission by change it

--- core/ProjectManager/project/permissions.py
+++ core/ProjectManager/project/permissions.py
@@ -3,4 +3,7 @@ from rest_framework.permissions import BasePermission
 
 class AddProjectPermission(BasePermission):
     def has_permission(self, request, view):
-        return request.user.position == "project manager"
+        if request.user.is_authenticated:
+            return request.user.position == "project manager"
+        else:
+            return False
